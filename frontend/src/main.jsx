03a5a8ffac7814544a2bad259709e6d3341c3aab
Shuqin2025 <shuqinamberg@gmail.com>
2025-08-28 23:19:40 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,10 @@
+// frontend/src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx'   // 如果你把 App 放在 components 里，请改为 './components/App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
